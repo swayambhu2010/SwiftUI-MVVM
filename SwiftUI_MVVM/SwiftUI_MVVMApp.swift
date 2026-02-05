1324ca167491f74b93b21cfd3a496c24998c38c0
Swayambhu Jyoti Banerjee <swayambhu.2010@gmail.com>
2026-02-05 18:11:45 +0530

    Initial Commit

---
+++ SwiftUI_MVVM/SwiftUI_MVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_MVVMApp.swift
+//  SwiftUI_MVVM
+//
+//  Created by Swayambhu BANERJEE on 05/02/26.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_MVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
