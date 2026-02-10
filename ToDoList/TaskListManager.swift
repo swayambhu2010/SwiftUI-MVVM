228b2d7b46d3fcf32d7a21c882aa61d48d6578c3
Swayambhu Jyoti Banerjee <swayambhu.2010@gmail.com>
2026-02-10 13:42:36 +0530

    TaskList Manager class added

---
+++ ToDoList/TaskListManager.swift
@@ -0,0 +1,47 @@
+//
+//  TaskListManager.swift
+//  SwiftUI_MVVM
+//
+//  Created by Swayambhu BANERJEE on 10/02/26.
+//
+
+import Foundation
+import Combine
+
+class TaskListManager: ObservableObject {
+    
+   @Published var items: [Task] = []
+    
+    init() {
+        self.items = getTaskList()
+    }
+    
+    private func refreshTaskList() {
+        guard let data = try? JSONEncoder().encode(items) else { return }
+        UserDefaults.standard.set(data, forKey: "notes")
+    }
+    
+    private func getTaskList() -> [Task] {
+        guard let data = UserDefaults.standard.data(forKey: "notes") else { return [] }
+        guard let json = try? JSONDecoder().decode([Task].self, from: data) else { return [] }
+        items = json
+        return items
+    }
+    
+    private func updateTaskList(task: Task) {
+        items.append(task)
+        refreshTaskList()
+    }
+    
+    private func deleteTask(task: Task) {
+        let index = items.firstIndex(of: task) ?? 0
+        items.remove(at: index)
+        refreshTaskList()
+    }
+    
+    private func toggleTaskCompletion(task: Task) {
+        let index = items.firstIndex(of: task) ?? 0
+        items[index].isCompleted.toggle()
+        refreshTaskList()
+    }
+}
